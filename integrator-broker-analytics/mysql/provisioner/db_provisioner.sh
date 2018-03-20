e7cbc5c473ba3e492e05c171c225305ec41a12db
chirangaalwis <chirangaalwis@gmail.com>
2018-03-21 00:32:10 +0530

    Fix synced folder issue in integrator-broker-analytics

--- integrator-broker-analytics/mysql/provisioner/db_provisioner.sh
+++ integrator-broker-analytics/mysql/provisioner/db_provisioner.sh
@@ -14,10 +14,11 @@
 
 DB_USER=root
 DB_PASSWORD=wso2carbon
+WORKING_DIRECTORY=/home/vagrant
 
 # run product db script
 echo "Execute the database scripts..."
-mysql -u${DB_USER} -p${DB_PASSWORD} -e "source /vagrant/mysql/provisioner/scripts/mysql.sql"
+mysql -u${DB_USER} -p${DB_PASSWORD} -e "source ${WORKING_DIRECTORY}/mysql/scripts/mysql.sql"
 echo "Successfully executed the database scripts."
 
 # grants root access to MySQL server from any host
